1f87f94149e8fdf8a89ed6f0546f910c112d339e
Grant Holmes <g.holmes429@gmail.com>
2022-04-10 06:12:38 -0500

    about info

--- semantic-frontend/src/components/about.jsx
+++ semantic-frontend/src/components/about.jsx
@@ -17,16 +17,16 @@ const AboutPage = () => {
         }}
       >
         <Typography variant="subtitle1" gutterBottom>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Vestibulum
-          sed arcu non odio euismod. Sem et tortor consequat id. Odio
-          pellentesque diam volutpat commodo. Diam quis enim lobortis
-          scelerisque fermentum dui faucibus in ornare. Sed viverra ipsum nunc
-          aliquet bibendum enim. Varius vel pharetra vel turpis nunc. Sed odio
-          morbi quis commodo odio. Aliquam vestibulum morbi blandit cursus risus
-          at. Enim blandit volutpat maecenas volutpat blandit aliquam etiam erat
-          velit. Mauris nunc congue nisi vitae suscipit tellus mauris. In
-          pellentesque massa placerat duis ultricies lacus sed turpis tincidunt.
+          We train our own custom word embeddings using the FastText algorithm on our corpus of over 80
+          gothic and romantic novels, poems, and other textual mediums from a time period of over 100
+          years. We can then find how "similar" terms are with each other by computing cosine similarity
+          between vectors representing different terms in our corpus' vocabulary. In this sense, we can compare
+          processed tokens from a query to relevant from documents to create a semnatically grounded prevalence score.
+          We must heavily preprocess the corpus, employing a variety of tokenizers, stemmers,
+          and other natural language processing tools. Basing our search engine on the classic
+          vector space model with inverted index, we can easily resolve the most relevant documents
+          to a given query. We combine these techniques to create an intuitve, interesting, and insightful presentation
+          of how semantic meaning changes over time for this influential movement.
         </Typography>
       </Box>
     </React.Fragment>
